a4ec1dde8f74bc1ab6f454048b377242c9fa8ac2
msguixe <monica.sg7@gmail.com>
2020-04-15 21:35:47 +0200

    Finished function task1

--- R/Task1.R
+++ R/Task1.R
@@ -4,150 +4,107 @@
 #output: Dataframe with samples, stageI or IV, years smoked
 
 task1<-function(cancer_type){
-  return(clinical.table)
+  
+    #load library
+    library(TCGAbiolinks)
+
+    ###
+    #1 GET patients with rna, cn and met data
+    ###
+    
+    query.rna <- GDCquery(project = cancer_type, 
+                          data.category = "Transcriptome Profiling", 
+                          data.type = "Gene Expression Quantification", 
+                          workflow.type = "HTSeq - FPKM-UQ")
+    query.cn <- GDCquery(project = cancer_type, 
+                         data.category = "Copy Number Variation", 
+                         data.type = "Masked Copy Number Segment")
+    query.met <- GDCquery(project = cancer_type, 
+                          data.category = "DNA Methylation", 
+                          platform = "Illumina Human Methylation 450",
+                          data.type = "Methylation Beta Value")
+    
+    common.patients <- intersect(substr(getResults(query.rna, cols = "cases"), 1, 12),
+                                 intersect(substr(getResults(query.cn, cols = "cases"), 1, 12),
+                                           substr(getResults(query.met, cols = "cases"), 1, 12)))
+    
+    
+    ###
+    #2 Get clinical data
+    ###
+    
+    clinical.data <- GDCquery_clinic(project = cancer_type, type = "clinical")
+    
+    common.patients <- as.data.frame(common.patients)
+    colnames(common.patients) <- "submitter_id"
+    
+    clinical.common.patients <- merge(clinical.data, common.patients, by = "submitter_id")
+    
+    dim(clinical.data)
+    dim(clinical.common.patients)
+    
+    ###
+    #3 Select patients with tumoral stage I and IV
+    ###
+    
+    #To get the column number of the columns of interest: patient_id, tumor stage and years smoked
+    id_col <- grep("submitter_id", colnames(clinical.common.patients))
+    ts_col <- grep("tumor_stage", colnames(clinical.common.patients))
+    ys_col <- grep("^years_smoked", colnames(clinical.common.patients))
+    
+    #To get up to 10 patients with stage I and 10 patients with stage II
+    stageI <- clinical.common.patients[grep("stage i$|stage ia|stage ib", clinical.common.patients$tumor_stage),c(id_col, ts_col, ys_col)]
+    
+    if (nrow(stageI) >= 10){
+      stageI <- stageI[1:10,]
+    }
+    
+    stageIV <- clinical.common.patients[grep("stage iv", clinical.common.patients$tumor_stage),c(id_col, ts_col, ys_col)]
+    
+    if (nrow(stageIV) >= 10){
+      stageIV <- stageIV[1:10,]
+    }
+    
+    #To change "stage ia" and "stage ib" to "stage i" (same for "stage iv")
+    
+    clinical.table <- rbind(stageI, stageIV)
+    clinical.table$tumor_stage <- as.factor(clinical.table$tumor_stage)
+    
+    levels(clinical.table$tumor_stage)[levels(clinical.table$tumor_stage)=="stage ia"] <- "stage i"
+    levels(clinical.table$tumor_stage)[levels(clinical.table$tumor_stage)=="stage ib"] <- "stage i"
+    levels(clinical.table$tumor_stage)[levels(clinical.table$tumor_stage)=="stage iva"] <- "stage iv"
+    levels(clinical.table$tumor_stage)[levels(clinical.table$tumor_stage)=="stage ivb"] <- "stage iv"
+    
+    #Prepare the final output
+    
+    rownames(clinical.table) <- c(1:nrow(clinical.table))
+    colnames(clinical.table) <- c("barcode", "tumor_stage", "years_smoked")
+
+return(clinical.table)
 }
 
-library(TCGAbiolinks)
+
+
+#########################
+#Test the function
+#########################
 
 ###
-#1
+#0 INPUT: cancer type
 ###
 
 #To get all the project id's from TCGA
 all_cancer_list <- TCGAbiolinks:::getGDCprojects()$project_id
-
-cancer_list <- cancer_list[grep("ESCA1|HNSC|KICH|KIRC|KIRP|LUAD|LUSC|SKCM|STAD", all_cancer_list)]
-
-cancer_list[1]
-
-#This is the input of the function
-cancer_type <- "TCGA-LUAD"
-
-#To check the datasets of each cancer type
-ESCAdatasets <- TCGAbiolinks:::getProjectSummary(cancer_list[1]) #ESCA
-
-ESCAdatasets$data_categories$data_category
-
-###
-#2
-###
-
-#Check that these samples have rna, cn and meth data:
-
-query.rna <- GDCquery(project = cancer_type, 
-                      data.category = "Transcriptome Profiling", 
-                      data.type = "Gene Expression Quantification", 
-                      workflow.type = "HTSeq - FPKM-UQ")
-query.cn <- GDCquery(project = cancer_type, 
-                     data.category = "Copy Number Variation", 
-                     data.type = "Masked Copy Number Segment")
-query.met <- GDCquery(project = cancer_type, 
-                      data.category = "DNA Methylation", 
-                      platform = "Illumina Human Methylation 450",
-                      data.type = "Methylation Beta Value")
+cancer_list <- all_cancer_list[grep("ESCA|HNSC|KICH|KIRC|KIRP|LUAD|LUSC|SKCM|STAD", all_cancer_list)]
+cancer_list
 
 
-# Get all patients that have DNA methylation and gene expression.
-common.patients <- intersect(substr(getResults(query.rna, cols = "cases"), 1, 12),
-                            intersect(substr(getResults(query.cn, cols = "cases"), 1, 12),
-                             substr(getResults(query.met, cols = "cases"), 1, 12)))
-
-length(common.patients)#457
-
-###
-#3
-###
-
-#query the clinical data:
-clinical.data <- GDCquery_clinic(project = cancer_type, type = "clinical")
-
-#To get the column number of the columns of interest: patient_id, tumor stage and years smoked
-id_col <- grep("submitter_id", colnames(clinical.data))
-ts_col <- grep("tumor_stage", colnames(clinical.data))
-ys_col <- grep("years_smoked", colnames(clinical.data))
-
-
-
-clinical.common.patients <- merge(clinical.data, common.patients, by =clinical.data$submitter_id)
-
-
-#To get 10 patients with stage I and 10 patients with stage II
-stageI <- clinical.data[grep("stage ia|stage ib", clinical.data$tumor_stage),c(id_col, ts_col, ys_col)]
-stageI <- stageI[1:10,] #implement an if to consider the case of less than 10 samples per group
-stageIV <- clinical.data[grep("stage iv|stage iv", clinical.data$tumor_stage),c(id_col, ts_col, ys_col)]
-stageIV <- stageIV[1:10,]
-
-clinical.table <- rbind(stageI, stageIV)
-clinical.table$tumor_stage <- as.factor(clinical.table$tumor_stage)
-
-levels(clinical.table$tumor_stage)[levels(clinical.table$tumor_stage)=="stage ia"] <- "stage i"
-levels(clinical.table$tumor_stage)[levels(clinical.table$tumor_stage)=="stage ib"] <- "stage i"
-
-
-##################################################################
-
-TCGA.chol.exp #class: RangedSummarizedExperiment 
-colData(TCGA.chol.exp)
-rowData(TCGA.chol.exp)
-rowRanges(TCGA.chol.exp)
-assays(TCGA.chol.exp)
-head(assay(TCGA.chol.exp))
-
-
-TCGA.chol.exp.mut <- GDCprepare(query,add.gistic2.mut = c("PTEN","FOXJ1"))
-TCGA.chol.exp.mut #information has been added to previous results
-colData(TCGA.chol.exp.mut)
-
-#copy number 
-# query <- GDCquery(project = "TCGA-CHOL", 
-#                   data.category = "Copy Number Variation",
-#                   data.type = "Copy Number Segment")
-# GDCdownload(query)  
-
-# query <- GDCquery(project = "TCGA-CHOL", 
-#                   data.category = "Copy number variation",
-#                   legacy=T,
-#                   platform="Affymetrix SNP Array 6.0",file.type="hg19.seg")
-# GDCdownload(query)  
-
-query <- GDCquery(project = "TCGA-CHOL", 
-                  data.category = "Copy Number Variation",
-                  data.type = "Masked Copy Number Segment")
-GDCdownload(query)  
-
-TCGA.chol.CN <- GDCprepare(query) 
-
-TCGA.chol.CN #tibble with alteraions by sample, chromosome, start and end
-class(TCGA.chol.CN)
-dim(TCGA.chol.CN)
-head(TCGA.chol.CN)
-
-#methylation data it tales long time
-# query <- GDCquery(project = "TCGA-CHOL", 
-#                   data.category = "DNA Methylation", 
-#                   platform = "Illumina Human Methylation 450")
-# GDCdownload(query)
-# TCGA.chol.meth <- GDCprepare(query) 
-# class(TCGA.chol.meth) #
-# dim(TCGA.chol.meth)
-# head(TCGA.chol.meth)
-
-#clinical data, the following code does not work
-# query <- GDCquery(project = "TCGA-CHOL",
-#                   data.category = "Clinical")
-# GDCdownload(query) 
-# TCGA.chol.clin <- GDCprepare(query)  #it does not work
-
-#instead use this
-clinical <- GDCquery_clinic(project = "TCGA-CHOL", type = "clinical")
-
-# Downloading and prepare using legacy
-query <- GDCquery(project = "TCGA-GBM",
-                  data.category = "Protein expression",
-                  legacy = TRUE, 
-                  barcode = c("TCGA-OX-A56R-01A-21-A44T-20","TCGA-08-0357-01A-21-1898-20"))
-GDCdownload(query)
-data <- GDCprepare(query, save = TRUE, 
-                   save.filename = "gbmProteinExpression.rda",
-                   remove.files.prepared = TRUE)
-                   
+LUAD.table <- task1(cancer_type = "TCGA-LUAD")
+KIRK.table <- task1(cancer_type = "TCGA-KIRC")
+HNSC.table <- task1(cancer_type = "TCGA-HNSC")
+STAD.table <- task1(cancer_type = "TCGA-STAD")
+LUSC.table <- task1(cancer_type = "TCGA-LUSC")
+KICH.table <- task1(cancer_type = "TCGA-KICH")
+SKCM.table <- task1(cancer_type = "TCGA-SKCM")
+KIRP.table <- task1(cancer_type = "TCGA-KIRP")
+ESCA.table <- task1(cancer_type = "TCGA-ESCA")

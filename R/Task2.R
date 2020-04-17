8a8865706f0126c6184b557190068b21e666ea01
ariadna.cilleros <ariadna.cilleros@uvic.cat>
2020-04-17 14:32:48 +0200

    commit

--- R/Task2.R
+++ R/Task2.R
@@ -1,8 +1,45 @@
-#Task 2
-#Purpose
-#input
-#output
-#start
+#Task 2: Alessia Squitieri 
+#Purpose: prepare mRNA data
+#input: dataframe of function1 
+#output: dataframe with normalized expression data 
+
+# PROOF function2
+##########################################################################
+library(TCGAbiolinks)
+library(edgeR)
+library(SummarizedExperiment)
+
 task2<-function(){
   return()
 }
+
+## defining variable of interest from the dataframe of function1 : 
+barcode <- LUAD.table$barcode
+cancer_type <- "TCGA-LUAD"
+
+# download quey.rna 
+query.rna <- GDCquery(project = cancer_type, 
+                      barcode = barcode,
+                      data.category = "Transcriptome Profiling", 
+                      data.type = "Gene Expression Quantification", 
+                      workflow.type = "HTSeq - FPKM-UQ")
+
+getResults(query.rna) #data that will be downloaded
+GDCdownload(query.rna)
+
+TCGA.exp <- GDCprepare(query.rna) #this is creating the R object
+
+# subset sample
+samples.exp <- substr(colnames(TCGA.exp),1,12)
+length(samples.exp) # we obtain 23 samples instead of 20, some there are some replicates: 
+length(unique(samples.exp))
+
+s <- colnames(TCGA.exp)[!duplicated(substring(colnames(TCGA.exp), 1, 12))]
+
+# matrix with cols = samples and rows = genes
+data <- assay(TCGA.exp)
+
+# to normalize data with edger we first created a DEGlist and then we used the function calcNormFactors:
+data <- DGEList(data)
+tmm <- calcNormFactors(data)
+
--- R/Task5.R
+++ R/Task5.R
@@ -1,7 +1,24 @@
 #Task 5 Óscar Baeza
-#Purpose
-#input
-#output
-task5<-function(){
-  return()
+#Purpose: Perform correlations (Spearman) (CN/mRNA and mRNA/methylation) and retrieve a table with good correlated genes and regression plots.
+#Input: 2 data.frames with format being samples in columns and genes (HUGO Symbol) in rows (mRNA data),
+# samples in columns and genes (HUGO Symbol) in rows containing the segment mean of each gene and sample (CN data),
+# samples in columns and genes (HUGO Symbol) in rows containing the beta values mean of each gene and sample (methilation data).
+#Output: data.frame with good correlations between data.frames (in absolute value >0.67)
+task5<-function(dataframe1, dataframe2, method){
+  return(correlations)
+}
+
+dataframe1 <- data.frame(mtcars)
+dataframe1 <- c("1","2")
+
+if (class(dataframe1) == "data.frame") {
+  print("It is a dataframe")
+  } else {
+    stop(paste(deparse(substitute(dataframe1)))," is not a data.frame")
+}
+
+
+method <- "CN/mRNA"
+if(method == "CN/mRNA") {
+  print("Hello")
 }

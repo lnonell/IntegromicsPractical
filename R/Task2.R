c7fe24c240f69c3cf3f9e97a3ac33e51fcf6d7cd
alessia677 <alessiasquitieri10@gmail.com>
2020-04-18 16:58:17 +0200

    Finished task2 function

--- R/Task2.R
+++ R/Task2.R
@@ -1,22 +1,26 @@
 #Task 2: Alessia Squitieri 
-#Purpose: prepare mRNA data
-#input: dataframe of function1 
-#output: dataframe with normalized expression data 
+#Purpose: Prepare expression data 
+#input: Dataframe of function1 and cancer code 
+#output: Dataframe with normalized expression data 
 
 ##########################################################################
-# PROOF function2
+# Function task2:
 ##########################################################################
+
 library(TCGAbiolinks)
 library(edgeR)
 library(SummarizedExperiment)
 library(biomaRt)
-##########################################################################
+
+## Example of cancer type: 
 cancer_type <- "TCGA-LUAD"
-clinical.table$barcode <- barcode 
+## Define barcode variable before using the function, from the clinical.table obtained by function1:
+barcode <- LUAD.table$barcode # clinical table obtained form function1 
 
 task2<-function(cancer_type, clinical.table){
   ########################################################################
-  # download quey.rna 
+  # 1. Download quey.rna:
+  ########################################################################
   query.rna <- GDCquery(project = cancer_type, 
                       barcode = barcode,
                       data.category = "Transcriptome Profiling", 
@@ -25,29 +29,51 @@ task2<-function(cancer_type, clinical.table){
   getResults(query.rna) #data that will be downloaded
   GDCdownload(query.rna)
   TCGA.exp <- GDCprepare(query.rna) #this is creating the R object
-  ########################################################################
-  # Annotation 
-  ensembl <- useMart("ensembl")
-  mart <- useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl", host="www.ensembl.org")
-  annot_df <- getBM(attributes = c("ensembl_gene_id","hgnc_symbol"), 
-                  filters = "ensembl_gene_id", values = rownames(TCGA.exp), mart = mart)
-  annot_df <- annot_df[order(annot_df[,2]),] #order by start position
-  genes <- annot_df[order(annot_df[,1]),] #order by chromosome
   
   ########################################################################
-  # Create matrix of interest and check for duplicates: 
+  # 2. Annotation HUGO:
+  ########################################################################
+  ensembl <- useMart("ensembl")
+  mart <- useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl", host="www.ensembl.org")
+  annot <- getBM(attributes = c("ensembl_gene_id","hgnc_symbol"), 
+                  filters = "ensembl_gene_id", values = rownames(TCGA.exp), mart = mart)
+  
+  ## Create expression matrix
   data <- assay(TCGA.exp)
-  patient <- substr(colnames(data),1,12)
+  
+  ## Check order 
+  data <- data[annot$ensembl_gene_id, ]
+  
+  ## Subset with the ones that have HGNC symbol
+  sub_data <- data[annot[annot$hgnc_symbol != "",]$ensembl_gene_id, ]
+  
+  rownames(sub_data) <- annot[annot$hgnc_symbol != "",]$hgnc_symbol
+  head(sub_data)
+
+  ########################################################################
+  # 3. Create matrix of interest and check for duplicates: 
+  ########################################################################
+
+  patient <- substr(colnames(sub_data),1,12)
   
   if(any(duplicated(patient)) == TRUE){
   dupl <- which(duplicated(patient))
-  data <- data[,-dupl]
+  sub_data <- sub_data[,-dupl]
   }
+  
+  dim(sub_data)
+  
   ########################################################################
-  # Normalization: 
-  data <- DGEList(data)
-  norm_data <- calcNormFactors(data)
+  # 4. Normalization: 
+  ########################################################################
+  sub_data <- DGEList(sub_data)
+  norm_data <- calcNormFactors(sub_data)
   expression.table <- as.data.frame(norm_data$counts)
   return(expression.table)
 }
+
+  ####################### To test the function ###########################
+  LUAD.exp <- task2(cancer_type, clinical.table = LUAD.table)
+  LUSC.exp <- task2(cancer_type = cancer_type, clinical.table = LUSC.table)
+  SKCM.exp <- task2(cancer_type, clinical.table = SKCM.table)
 

4002bca1ba8814d019dd289d78098a83be3e3609
ariadna.cilleros <ariadna.cilleros@uvic.cat>
2020-04-25 21:21:38 +0200

    commit(25/04)

--- R/Task3.R
+++ R/Task3.R
@@ -2,13 +2,14 @@
 #Purpose: Download, preapre and treat CNV data.
 #input: Dataframe with the metadata realted to our samples (output task1) and TCGA cancer type (ex: TCGA-KIRC)
 #output: Dataframe with the CNV data related to each gene and grouped by patient. 
-#         The data has been converted to be more suitable: -1, 0, +1.
+#         The data will be converted by default (transform = TRUE) converted to be more suitable: -1, 0, +1.
 
-task3<- function(cancer, df_samples){
+task3<- function(cancer, df_samples, transform = TRUE){
   
   #check arguments  
   stopifnot(is.character(cancer))
   stopifnot(is.data.frame(df_samples))
+  stopifnot(is.logical(transform))
   
   suppressPackageStartupMessages(library(TCGAbiolinks))
   suppressPackageStartupMessages(library(GenomicFeatures))
@@ -67,28 +68,31 @@ task3<- function(cancer, df_samples){
   #overlap the annotation with our CNV probes
   hits <- findOverlaps(genes_GR, df_GR, type="within") #hits found
   df_ann <- cbind(TCGA.CN[subjectHits(hits),],genes[queryHits(hits),]) 
-  df_ann$Patient<-gsub(pattern = "\\b.\\b", replacement = "-", substr(df_ann$Sample,1,12)) #create patient variable
-  df_ann <- df_ann[,c(2,3,4,6,11,12)] #take chr, start, end, segment_mean, HGNC and Patient columns
+  df_ann$Patient<-substr(df_ann$Sample,1,12) #create patient variable
+  df_ann <- df_ann[,c(2,3,4,6,7,11,12)] #take chr, start, end, segment_mean, HGNC, patient and sample columns
   
   
-  #At this step we can face three problems: 1) having patients with more than one sample,
-  #2) having more than one segment that measures the same gene, and 3) having patients
+  #At this step we can face three problems: 1) having more than one segment that measures 
+  #the same gene 2) having patients with more than one sample, and 3) having patients
   #with missing values for a fragment. For the first problem, we will allways get the 
   #first sample per patient, for the second we will perform the mean between the 
   #different values assigned to the same gene and to the same sample, and for the third 
   #problem, we will assign a 0, assuming that this patient hasn't have alteration on that gene. 
   
-  for (gene in unique(df_ann$GeneSymbol)){
+  for (gene in unique(df_ann$GeneSymbol)[1]){
     gene.CN<-df_ann[df_ann$GeneSymbol==gene,]
+    gene.CN.bySam <- data.frame()
     gene.CN.byPat <- data.frame()
     
-    #1) Discard duplicates
-    for (pat in unique(gene.CN$Patient)){
-      gene.CN.byPat <- rbind(gene.CN.byPat, gene.CN[gene.CN$Patient == pat, c(6,4)][1,])
-    }
+    #1. Take the mean of the samples that can have more than one segment mean per gene 
+    gene.CN.bySam <- gene.CN %>% group_by(Sample) %>% summarise(CN = mean(Segment_Mean, na.rm = TRUE))
+    colnames(gene.CN.bySam) <- c("Sample",gene)
+    gene.CN.bySam$Sample <- substr(gene.CN.bySam$Sample,1,12) #now we pass from samples to patients
     
-    #2)Summarize the value of a gene doing the mean, taking in count that we are grouping per samples
-    gene.CN.byPat <- gene.CN.byPat %>% group_by(Patient) %>% summarise(CN = mean(Segment_Mean, na.rm = TRUE))
+    #2. Take only one sample per patient in case of having duplicates
+    for (patient in unique(gene.CN.bySam$Sample)){
+      gene.CN.byPat <- rbind(gene.CN.byPat, gene.CN[gene.CN$Patient == patient, c(7,4)][1,])
+    }
     colnames(gene.CN.byPat) <- c("Patient",gene)
     
     #3) In case that at least one patient is missing
@@ -100,7 +104,7 @@ task3<- function(cancer, df_samples){
       }
     }
     
-    #Make sure that we have the same order for patients in the data frame
+    #Make sure that we have the same order for patients in the data frame 
     gene.CN.byPat <- as.data.frame(gene.CN.byPat[match(gene.CN.byPat$Patient, unique(df_ann$Patient)),]) 
     
     #With the first gene we will create the dataframe to store all the results
@@ -116,16 +120,17 @@ task3<- function(cancer, df_samples){
   
   ###########################
   ##
-  ## STEP5: Transform data
+  ## STEP5: Preprocess final dataframe 
   ##
   ###########################
   
-  #We will transform the values to -1, 0 and 1, being -1 less copies of the gene, 
-  #0 the normal copies, and +1 more copies than the expected. 
-  
   df_CN <- t(df_CN) #we will transpose to have genes in rows and patients in columns 
   
-  for (i in 1:(dim(df_CN)[2])) df_CN[,i] <- as.integer(ifelse(2^(as.numeric(df_CN[,i])+1)>2.4,1,ifelse(2^(as.numeric(df_CN[,i])+1)<1.6,-1,0)))
+  #We will transform the values to -1, 0 and 1, being -1 less copies of the gene, 
+  #0 the normal copies, and +1 more copies than the expected. 
+  if (transform == TRUE){
+    for (i in 1:(dim(df_CN)[2])) df_CN[,i] <- as.integer(ifelse(2^(as.numeric(df_CN[,i])+1)>2.4,1,ifelse(2^(as.numeric(df_CN[,i])+1)<1.6,-1,0)))
+  }
   
   return(data.matrix(data.frame(df_CN, stringsAsFactors = FALSE))) #let's make sure that we get a numeric matrix
   

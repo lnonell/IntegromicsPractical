52848a875000f5654ec01d40436a311c63d31f08
Cristina Tuñí i Domínguez <c.tuni.dominguez@gmail.com>
2020-05-05 16:06:07 +0200

    FInding Overlaps

--- R/Task4.R
+++ R/Task4.R
@@ -1,3 +1,5 @@
+cancer = "TCGA-LUAD"
+df_samples = LUAD.pts
 #Task 4: Cristina Tuni i Dominguez
 #Purpose: Prepare methylation data to be downloaded and further treated.
 #input: Data frame with the prepared clinical data from task 1, plus the cancer code from TCGA.
@@ -39,14 +41,40 @@ task4<-function(cancer, df_samples){
   #get genes information
   genes.info<- rowRanges(TCGA.meth)
   
+  #get sample information
+  sample.info<- colData(TCGA.meth)
+  
+  ########################################################
+  #2. Annotation
+  ########################################################
+  
+  #Set up an gene annotation template to use
+  txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
+  genes <- genes(txdb)
+  ensembl <- useMart("ensembl")
+  mart <- useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl", host="www.ensembl.org")
+  
+  #obtain chr, start, and HGNC name of all the genes annotated in hg38
+  annot_df <- getBM(attributes = c("chromosome_name","start_position","end_position","hgnc_symbol"), 
+                    filters = "entrezgene_id", values = genes$gene_id, mart = mart)
+  
+  annot_df<- annot_df[annot_df[,4]!="" & annot_df[,1] %in% c(1:22,"X","Y"),] #remove those not annotated 
+  annot_df <- annot_df[order(annot_df[,2]),] #order by start position
+  genes <- annot_df[order(annot_df[,1]),] #order by chromosome
+  
+  ########################################################
+  #3. Data processing
+  ########################################################
+  
   #check if the data object and the genes.info object have the same order (they should)
   all(genes.info@ranges@NAMES==rownames(data)) #TRUE
   
   #from the previous GRanges, obtain useful dataframe
-  rownames(genes.info)==rownames(data)
   
-  chromosome<-genes.info@seqnames
-  start_position<-genes.info@ranges@start
+  chr<-genes.info@seqnames
+  
+  start<-as.double(genes.info@ranges@start)
+  end<-as.double(start_position+1)
   
   gene_name<-genes.info@elementMetadata@listData$Gene_Symbol
   i=0
@@ -61,15 +89,41 @@ task4<-function(cancer, df_samples){
   }
   
   
-  genes.info_df<-data.frame(chromosome,start_position, gene_name, position_to_TSS, data)
-  dim(genes.info_df) #485577 27
-  head(genes.info_df)
+  genes.info_df<-as_tibble(data.frame(chr,start, end, gene_name, position_to_TSS, data))
   
   #remove those probes without annotated genes
   genes.info_df<-genes.info_df[genes.info_df$gene_name != ".", ]
   
+  
+  #operations to drop the first three chracters in the chromosome column
+  genes.info_df$chr<-unfactor(genes.info_df$chr)
+  
+  dropchr <- function(x) {  
+    substr(x,4,4)}
+  
+  genes.info_df$chr<-sapply(genes.info_df$chr, dropchr)
+  
   #transform the positions to numeric type
   genes.info_df$position_to_TSS<-as.numeric(as.character(genes.info_df$position_to_TSS))
+  
+  
+  ########################################################
+  #4. Finding overlaps
+  ########################################################
+  
+  #make GRanges from annotation
+  colnames(genes) <- c("chr","start", "end", "GeneSymbol")
+  genes$start<-genes$start-3000
+  genes_GR <- makeGRangesFromDataFrame(genes,keep.extra.columns = TRUE)
+  
+  #make GRanges object from the tibble
+  df_GR <- makeGRangesFromDataFrame(genes.info_df[,c(1,2,3)])#we only take chr, start and end to do the overlap
+  
+  #overlap the annotation with the cpgs
+  hits <- findOverlaps(df_GR,genes_GR, type="within") #hits found
+  
+  df_ann <- cbind(genes.info_df[subjectHits(hits),],genes[queryHits(hits),]) 
+  
   
   #aggregated data frame
   first_sample<-aggregate(TCGA.05.4390.01A.02D.1756.05 ~ gene_name, FUN = mean, data=genes.info_df)
@@ -83,13 +137,8 @@ task4<-function(cancer, df_samples){
   #transform the positions to numeric type
   genes.info_df$position_to_TSS<-as.numeric(as.character(genes.info_df$position_to_TSS))
   
-  #get sample information
-  sample.info<- colData(TCGA.meth)
   
-  ########################################################
-  #2. Annotation
-  ########################################################
- 
+  
   
   return()
 }

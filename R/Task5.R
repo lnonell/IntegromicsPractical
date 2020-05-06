5c8b7c69bf056998e33b0e00cd29a28182efbc2c
obaeza16 <oscarmaremar@gmail.com>
2020-05-06 18:00:11 +0200

    Duplicate funcion, one for CNV and the other for methilation. Other adjustments

--- R/Task5.R
+++ R/Task5.R
@@ -6,36 +6,47 @@
 #Output: data.frame with good correlations between data.frames (in absolute value >0.67)
 
 
-task5 <- function(df_mRNA, df_other, df_samples, num_plots, rhovalue_sig = 0.67, pvalue_sig = 0.05){
+
+#########################
+### CNV Correlations ###
+#########################
+
+
+task5.cnv <- function(df_mRNA, df_CNV, df_samples, num_plots, rhovalue_sig = 0.67, pvalue_sig = 0.05){
   
   # Check arguments  
   stopifnot(is.data.frame(df_mRNA))
-  stopifnot(is.data.frame(df_other))
+  stopifnot(is.data.frame(df_CNV))
   stopifnot(is.data.frame(df_samples))
   stopifnot(is.numeric(num_plots))
   
   # Libary loading
-  suppressPackageStartupMessages(library(TCGAbiolinks))
   suppressPackageStartupMessages(library(GenomicFeatures))
   suppressPackageStartupMessages(library(tidyverse))
   
   # We put all samples on same format
   colnames(df_mRNA) <- gsub("-",".",colnames(df_mRNA))
+  colnames(df_CNV) <- gsub("-",".",colnames(df_CNV))
   
   # To check if all samples are in the same order and correct it
-  if (all(colnames(df_mRNA)==colnames(df_other)) == FALSE) {
-    df_mRNA.s<-df_mRNA[,colnames(df_other)]
+  if (all(colnames(df_mRNA)==colnames(df_CNV)) == FALSE) {
+    df_mRNA.s<-df_mRNA[,colnames(df_CNV)]
     cat("\nThe samples have been reorganized...\n")
   }
   
+  # We remove any NaN data from both dataframes
+  df_meth <- df_meth[complete.cases(df_meth), ]
+  df_mRNA <- df_mRNA[complete.cases(df_mRNA), ]
+  
+  
   # We get all the common genes in both dataframes sorted
-  common.genes <- intersect(sort(rownames(df_other)),sort(rownames(df_mRNA.s)))
-  df_other.c <- df_other[common.genes,] # We select common rows in df other  
+  common.genes <- intersect(sort(rownames(df_CNV)),sort(rownames(df_mRNA.s)))
+  df_CNV.c <- df_CNV[common.genes,] # We select common rows in df other  
   df_mRNA.c <- df_mRNA.s[common.genes,] # We select common rows in df mRNA
   
   # We create the Results directory, hanging from the working directory
-  cat("\nCreating /Results directory...\n")
-  results_dir<-"/Results"
+  cat("\nCreating Results_CNV directory...\n")
+  results_dir<-"/Results_CNV"
   dir.create(file.path(getwd(), results_dir),showWarnings = FALSE)
   
   cat("\nPerforming correlations. This might take a while...\n")
@@ -53,7 +64,7 @@ task5 <- function(df_mRNA, df_other, df_samples, num_plots, rhovalue_sig = 0.67,
     for (j in 1:lng){
       gene<-common.genes[j]
       gene.id<-rownames(df_mRNA.c[rownames(df_mRNA.c) == gene,])[1]
-      y <- as.vector(unlist(df_other.c[gene.id,]))
+      y <- as.numeric(unlist(df_CNV.c[gene.id,]))
       x <- as.vector(unlist(df_mRNA.c[gene.id,]))
       cor<-cor.test(x,y, method = "spearman",exact=FALSE)
       cor.pval[j]<-cor$p.value
@@ -83,6 +94,8 @@ task5 <- function(df_mRNA, df_other, df_samples, num_plots, rhovalue_sig = 0.67,
   # We subset from the original correlation table to get significance
   cor.sig.table <- subset(x = cor.table, abs(cor.table$Rho) >= rhovalue_sig & cor.table$pval.adj < pvalue_sig)
   
+  # We order this table so we have the better adj.p value on top
+  cor.sig.table <- cor.sig.table[order(cor.sig.table$pval.adj),]
   
   # We make the plots for only those that are significative and selecting the number by argument
   genes.sig <- rownames(cor.sig.table)
@@ -94,9 +107,9 @@ task5 <- function(df_mRNA, df_other, df_samples, num_plots, rhovalue_sig = 0.67,
     # Loop for doing png plots per gene
     for (j in 1:lng){
       gene<-genes.sig[j]
-      gene.id<-rownames(KIRP.exp.c[rownames(KIRP.exp.c) == gene,])[1]
-      y <- as.vector(unlist(KIRP.cnv.c[gene.id,]))
-      x <- as.vector(unlist(KIRP.exp.c[gene.id,]))
+      gene.id<-rownames(df_mRNA.c[rownames(df_mRNA.c) == gene,])[1]
+      y <- as.vector(unlist(df_CNV.c[gene.id,]))
+      x <- as.vector(unlist(df_mRNA.c[gene.id,]))
       png(file.path(paste(getwd(),results_dir,paste(gene,"_mRNA_CNV_correlations.png",sep = ""),sep="/")))
       plot(y,x,main=gene,xlab="Copy Number Variation",ylab="RNA expression",type="b")
       dev.off()
@@ -104,9 +117,129 @@ task5 <- function(df_mRNA, df_other, df_samples, num_plots, rhovalue_sig = 0.67,
   } else {
     cat("\nOnly",length(genes.sig),"plots can be generated, and you asked for",num_plots,"\n")
   }
-  
   # Final function return
-  cat("\n DONE \n")
-  return(cor.sig.table)  
+  cat("\n DONE \n") 
+  return(cor.sig.table)
 }  
 
+
+
+################################
+### Methilation Correlations ###
+################################
+
+
+
+task5.meth <- function(df_mRNA, df_meth, df_samples, num_plots, rhovalue_sig = 0.67, pvalue_sig = 0.05){
+  
+  # Check arguments  
+  stopifnot(is.data.frame(df_mRNA))
+  stopifnot(is.data.frame(df_meth))
+  stopifnot(is.data.frame(df_samples))
+  stopifnot(is.numeric(num_plots))
+  
+  # Libary loading
+  suppressPackageStartupMessages(library(GenomicFeatures))
+  suppressPackageStartupMessages(library(tidyverse))
+  
+  # We put all samples on same format
+  colnames(df_mRNA) <- gsub("-",".",colnames(df_mRNA))
+  
+  # We erase the totalmean column from the data.frame if it exists
+  if ("totalmean" %in% colnames(df_meth) == TRUE) {
+    df_meth.nototal <- subset(df_meth, select=-c(totalmean))
+    cat("\nErasing totalmean column...\n")
+  }
+  
+  # To check if all samples are in the same order and correct it
+  if (all(colnames(df_mRNA)==colnames(df_meth.nototal)) == FALSE) {
+    df_mRNA.s<-df_mRNA[,colnames(df_meth)]
+    cat("\nThe samples have been reorganized...\n")
+  }
+  
+  
+  # We remove any NaN data from both dataframes
+  df_meth <- df_meth.nototal[complete.cases(df_meth.nototal), ]
+  df_mRNA <- df_mRNA[complete.cases(df_mRNA), ]
+  
+  # We get all the common genes in both dataframes sorted
+  common.genes <- intersect(sort(rownames(df_meth)),sort(rownames(df_mRNA)))
+  df_meth.c <- df_meth[common.genes,] # We select common rows in df other  
+  df_mRNA.c <- df_mRNA[common.genes,] # We select common rows in df mRNA
+  
+
+  # We create the Results directory, hanging from the working directory
+  cat("\nCreating /Results_Meth directory...\n")
+  results_dir<-"/Results_Meth"
+  dir.create(file.path(getwd(), results_dir),showWarnings = FALSE)
+  
+  cat("\nPerforming correlations. This might take a while...\n")
+  # Now we will perform the correlations 
+  # Initialize the variables to store pvalues and rho coefficient
+  lng <- length(common.genes)
+  if (lng>0) {
+    cor.rho<-array(NA,lng)
+    cor.pval<-array(NA,lng)
+    
+    
+    # Loop for doing correlations per gene, storing the rho and pvalue
+    for (j in 1:lng){
+      gene<-common.genes[j]
+      gene.id<-rownames(df_mRNA.c[rownames(df_mRNA.c) == gene,])[1]
+      y <- as.vector(unlist(df_meth.c[gene.id,]))
+      x <- as.vector(unlist(df_mRNA.c[gene.id,]))
+      cor<-cor.test(x,y, method = "spearman",exact=FALSE)
+      cor.pval[j]<-cor$p.value
+      cor.rho[j]<-cor$estimate
+      
+    }
+    
+  }
+  
+  # Assemble the correlation table
+  cat("\nAssembling correlation table...\n")
+  cor.table<-data.frame(row.names = common.genes,"Rho"=as.vector(cor.rho),"pval"=as.vector(cor.pval))
+  rownames(cor.table) <- common.genes
+  
+  # We adjust the pvalues for multiple comparisons
+  cat("\nAdjusting pvalues for multiple comparisons...\n")
+  pval.adj <- p.adjust(cor.table$pval,method = "fdr")
+  cor.table$pval.adj <- as.vector(pval.adj)
+  
+  # We make the Pvalue plots
+  cat("\nCreating PDFs for the Pvalue plots...\n")
+  pdf(file.path(paste(getwd(),results_dir,"pvalue_plots.pdf",sep="/")))
+  qqplot(x = cor.table$pval,y = cor.table$Rho, main = "P-value vs Rho plot")
+  qqplot(x = cor.table$pval.adj,y = cor.table$Rho, main = "Adjusted P-value vs Rho plot")
+  dev.off()
+  
+  # We subset from the original correlation table to get significance
+  cor.sig.table <- subset(x = cor.table, abs(cor.table$Rho) >= rhovalue_sig & cor.table$pval.adj < pvalue_sig)
+  
+  # We order this table so we have the better adj.p value on top
+  cor.sig.table <- cor.sig.table[order(cor.sig.table$pval.adj),]
+  
+  # We make the plots for only those that are significative and selecting the number by argument
+  genes.sig <- rownames(cor.sig.table)
+  
+  if (length(genes.sig)>= num_plots) {
+    lng <- length(genes.sig[1:num_plots])
+    
+    cat("\nCreating PNGs for the plots...\n")
+    # Loop for doing png plots per gene
+    for (j in 1:lng){
+      gene<-genes.sig[j]
+      gene.id<-rownames(df_mRNA.c[rownames(df_mRNA.c) == gene,])[1]
+      y <- as.vector(unlist(df_meth.c[gene.id,]))
+      x <- as.vector(unlist(df_mRNA.c[gene.id,]))
+      png(file.path(paste(getwd(),results_dir,paste(gene,"_mRNA_Meth_correlations.png",sep = ""),sep="/")))
+      plot(y,x,main=gene,xlab="Methilation Beta values",ylab="RNA expression",type="b",xlim=c(0,5000))
+      dev.off()
+    }
+  } else {
+    cat("\nOnly",length(genes.sig),"plots can be generated, and you asked for",num_plots,"\n")
+  }
+  # Final function return
+  cat("\n DONE \n") 
+  return(cor.sig.table)
+}
